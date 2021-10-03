245bdcc6da69bd3035fdda38642e4d1d3625d340
kandeepan <rpakeerathan60@gmail.com>
2021-10-03 15:59:06 +0530

    10/03/2021

---
+++ src/components/Student/MyProfileStudent.js
@@ -0,0 +1,226 @@
+import React, { Component } from 'react';
+import { propTypes } from 'react-bootstrap/esm/Image';
+import { getStudentByEmail,updateStudent } from "../../actions/studentActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import {Button, ButtonToolbar} from "react-bootstrap";
+
+ class UpdateStudent extends Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            firstName: "",
+            studentid: "",
+            mobileNumber: "",
+            email:"",
+            lastName:"",
+            hostelId:"",
+            address:"",
+            department:"",
+            faculty:"",
+            year:"",
+            gender:"",
+            
+      
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps){
+        const{
+            firstName,
+            studentid,
+            mobileNumber,
+            email,
+            lastName,
+            hostelId,
+            address,
+            department,
+            faculty,
+            year,
+            gender,
+            
+        } = nextProps.student;
+
+       this.setState({
+        firstName,
+        studentid,
+        mobileNumber,
+        email,
+        lastName,
+        hostelId,
+        address,
+        department,
+        faculty,
+        year,
+        gender,
+       
+
+       });
+
+    } 
+
+
+     componentDidMount(){
+        
+         const { email } = this.props.match.params;
+         this.props.getStudentByEmail(email);
+     }
+
+     onChange(e){
+         this.setState({[e.target.name]: e.target.value });
+     }
+
+     onSubmit(e) {
+         e.preventDefault();
+
+         const updateStudent = {
+
+            firstName: this.state.firstName,
+            studentid: this.state.studentid,
+            mobileNumber: this.state.mobileNumber,
+            email:this.state.email,
+            lastName:this.state.lastName,
+            hostelId:this.state.hostelId,
+            address:this.state.address,
+            mobileNumber:this.state.mobileNumber,
+            department:this.state.department,
+            faculty:this.state.faculty,
+            year:this.state.year,
+            gender:this.state.gender,
+            
+         };
+         this.props.updateStudent(updateStudent,this.props.history);
+     }
+    render() {
+        return (
+            <div className="project">
+            <div className="container">
+            <div className="row" style={{width:'800px', margin:'0 auto' ,marginTop:'10px'}}>
+                <div className="col-md-8 m-auto">
+                    <h5 className="display-4 text-center">My Details</h5>
+                    <hr />
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg "
+                             placeholder="student Id"
+                             name="studentid"
+                             value={this.state.studentid} 
+                             onChange={this.onChange}/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg" 
+                            placeholder="First Name"
+                            name="firstName"
+                            value={this.state.firstName}
+                            onChange={this.onChange}/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg"
+                             placeholder="Last Name"
+                             name="lastname"
+                             value={this.state.lastName}
+                             onChange={this.onChange}/>
+                        </div>
+                        <div className="form-group">
+                        <input type="text" className="form-control form-control-lg"
+                         placeholder="Mobile Number"
+                         name="mobileNumber"
+                         value={this.state.mobileNumber}
+                         onChange={this.onChange}/>
+                    </div>
+                    <div className="form-group">
+                    <input type="text" className="form-control form-control-lg"
+                     placeholder="Email"
+                     value={this.state.email}
+                     onChange={this.onChange}
+                     name="email"
+                     />
+                     <div className="form-group">
+                        <input type="text" className="form-control form-control-lg"
+                         placeholder="Address"
+                         name="address"
+                         value={this.state.address}
+                         onChange={this.onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control form-control-lg"
+                         placeholder="Department"
+                         name="department"
+                         value={this.state.department}
+                         onChange={this.onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control form-control-lg"
+                         placeholder="Faculty"
+                         name="faculty"
+                         value={this.state.faculty}
+                         onChange={this.onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control form-control-lg"
+                         placeholder="Year"
+                         name="year"
+                         value={this.state.year}
+                         onChange={this.onChange}/>
+                    </div>
+                </div>
+                <div className="form-group">
+                <input type="text" className="form-control form-control-lg"
+                 placeholder="Hostel Id"
+                 name="hostelId"
+                 value={this.state.hostelId}
+                 onChange={this.onChange}/>
+                </div>
+                 
+        <div className="form-button-group">
+        <ButtonToolbar>
+            
+            <Button
+                bsSize="small"
+                style={
+                    {   
+                        "width": "30%"
+                    }}
+                type="submit"
+            >
+                Update
+            </Button>
+            
+            <Button
+                bsSize="small"
+                style={{"width": "30%","backgroundColor": "#999",}}
+                type="button"
+                onClick={() => {}}
+            >
+                Cancel
+            </Button>
+        </ButtonToolbar>
+    </div>
+          </form>
+                </div>
+            </div>
+        </div>
+    </div>
+        )
+    }
+}
+
+UpdateStudent.propTypes = {
+    getStudentByEmail: PropTypes.func.isRequired,
+    updateStudent:PropTypes.func.isRequired,
+    student: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    student: state.student.student
+});
+
+export default connect(
+    mapStateToProps,
+    {getStudentByEmail,updateStudent} 
+    )(UpdateStudent);
---
+++ src/components/Warden/MyProfileWarden.js
@@ -0,0 +1,185 @@
+import React, { Component } from 'react';
+import { propTypes } from 'react-bootstrap/esm/Image';
+import { getWardenByEmail,updateWarden } from "../../actions/wardenActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import {Button, ButtonToolbar} from "react-bootstrap";
+
+ class UpdateWarden extends Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            firstName: "",
+            wardenid: "",
+            mobileNumber: "",
+            email:"",
+            lastName:"",
+            hostelId:"",
+            password:""
+      
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps){
+        const{
+            firstName,
+            wardenid,
+            mobileNumber,
+            email,
+            lastName,
+            hostelId,
+            password
+        } = nextProps.warden;
+
+       this.setState({
+        firstName,
+        wardenid,
+        mobileNumber,
+        email,
+        lastName,
+        hostelId,
+        password
+
+       });
+
+    } 
+
+
+     componentDidMount(){
+        
+         const { email } = this.props.match.params;
+         this.props.getWardenByEmail(email);
+     }
+
+     onChange(e){
+         this.setState({[e.target.name]: e.target.value });
+     }
+
+     onSubmit(e) {
+         e.preventDefault();
+
+         const updateWarden = {
+
+            firstName: this.state.firstName,
+            wardenid: this.state.wardenid,
+            mobileNumber: this.state.mobileNumber,
+            email:this.state.email,
+            lastName:this.state.lastName,
+            hostelId:this.state.hostelId,
+            password:this.state.password
+         };
+         this.props.updateWarden(updateWarden,this.props.history);
+     }
+    render() {
+        return (
+            <div className="project">
+            <div className="container">
+            <div className="row" style={{width:'800px', margin:'0 auto' ,marginTop:'10px'}}>
+                <div className="col-md-8 m-auto">
+                    <h5 className="display-4 text-center">My Details</h5>
+                    <hr />
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg "
+                             placeholder="Warden Id"
+                             name="wardenid"
+                             value={this.state.wardenid} 
+                             onChange={this.onChange}/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg" 
+                            placeholder="First Name"
+                            name="firstName"
+                            value={this.state.firstName}
+                            onChange={this.onChange}/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg"
+                             placeholder="Last Name"
+                             name="lastname"
+                             value={this.state.lastName}
+                             onChange={this.onChange}/>
+                        </div>
+                        <div className="form-group">
+                        <input type="text" className="form-control form-control-lg"
+                         placeholder="Mobile Number"
+                         name="mobileNumber"
+                         value={this.state.mobileNumber}
+                         onChange={this.onChange}/>
+                    </div>
+                    <div className="form-group">
+                    <input type="text" className="form-control form-control-lg"
+                     placeholder="Email"
+                     name="email"
+                     value={this.state.email}
+                     onChange={this.onChange}/>
+                </div>
+                <div className="form-group">
+                <input type="text" className="form-control form-control-lg"
+                 placeholder="Hostel Id"
+                 name="hostelId"
+                 value={this.state.hostelId}
+                 onChange={this.onChange}/>
+            </div>
+            <div className="form-group">
+            <input type="text" className="form-control form-control-lg" 
+            placeholder="Password"
+            name="password"
+            value={this.state.password}
+            onChange={this.onChange}/>
+        </div>
+                        
+        <div className="form-button-group">
+        <ButtonToolbar>
+            
+            <Button
+                bsSize="small"
+               
+                style={
+                    {
+                        
+                        "width": "30%"
+                    }}
+                type="submit"
+            >
+                Update
+            </Button>
+            
+            <Button
+                bsSize="small"
+                style={{"width": "30%","backgroundColor": "#999",}}
+                type="button"
+                onClick={() => {}}
+            >
+                Cancel
+            </Button>
+        </ButtonToolbar>
+    </div>
+          </form>
+                </div>
+            </div>
+        </div>
+    </div>
+        )
+    }
+}
+
+UpdateWarden.propTypes = {
+    getWardenByEmail: PropTypes.func.isRequired,
+    updateWarden:PropTypes.func.isRequired,
+    warden: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    warden: state.warden.warden
+});
+
+export default connect(
+    mapStateToProps,
+    {getWardenByEmail,updateWarden} 
+    )(UpdateWarden);
